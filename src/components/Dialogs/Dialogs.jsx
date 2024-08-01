56378ec155a178e1ea2edafd7ba98c5137da273a
Dmytro Boiko <iamboikodmytro@gmail.com>
2024-08-01 17:03:21 +0300

    add in dialogs users and messages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -6,6 +6,25 @@ const Dialogs = (props) => (
     <div className={style.bg}></div>
     <div className={style.content}>
       <h2 className={style.title}>Dialogs</h2>
+
+      <section className={style.wrapper}>
+        <section className={style.users}>
+          <article className={style.user}>Dima</article>
+          <article className={style.user}>Valera</article>
+          <article className={style.user}>Sveta</article>
+          <article className={style.user}>Dima</article>
+          <article className={style.user}>Valera</article>
+          <article className={style.user}>Sveta</article>
+        </section>
+        <section className={style.messages}>
+          <article className={style.message}>Hi!</article>
+          <article className={style.message}>How are you?</article>
+          <article className={style.message}>Hello, i'm here</article>
+          <article className={style.message}>Hi!</article>
+          <article className={style.message}>How are you?</article>
+          <article className={style.message}>Hello, i'm here</article>
+        </section>
+      </section>
     </div>
   </section>
 );
