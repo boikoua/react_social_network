67fec8fd22b6a57fbe968de84307e8d01c77ed7c
Dmytro Boiko <iamboikodmytro@gmail.com>
2024-08-07 09:23:35 +0300

    install redux in the project

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // Функции обновления стейта
-import store from './data/state';
+import store from './data/store';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
